458ca4fc46460dd96143fde8bb3fa188f2bdf00c
Neeraj Keriya <neerajkeriya@gmail.com>
2022-05-11 16:35:15 +0530

    Added Pagination feature to app

--- src/components/News.js
+++ src/components/News.js
@@ -7,18 +7,47 @@ export default class News extends Component {
     this.state = {
       articles: [],
       loading: false,
+      page: 1,
     };
   }
 
   async componentDidMount() {
-    let url =
-      "https://newsapi.org/v2/top-headlines?country=in&apiKey=72638e89521642eeae6d41e5f196d221&pageSize=10";
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=72638e89521642eeae6d41e5f196d221&pageSize=10&page=${this.state.page}`;
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
       articles: parsedData.articles,
+      totalArticles: parsedData.totalResults,
     });
   }
+
+  handlePrevEvent = async () => {
+    if (this.state.page > 1) {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=72638e89521642eeae6d41e5f196d221&pageSize=10&page=${
+        this.state.page - 1
+      }`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      this.setState({
+        articles: parsedData.articles,
+        page: this.state.page - 1,
+      });
+    }
+  };
+
+  handleNextEvent = async () => {
+    if (this.state.page <= Math.ceil(this.state.totalArticles / 10)) {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=72638e89521642eeae6d41e5f196d221&pageSize=10&page=${
+        this.state.page + 1
+      }`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      this.setState({
+        articles: parsedData.articles,
+        page: this.state.page + 1,
+      });
+    }
+  };
 
   render() {
     return (
@@ -38,6 +67,27 @@ export default class News extends Component {
             );
           })}
         </div>
+        <div className="container d-flex justify-content-between m-2 ">
+          <button
+            type="button"
+            className="btn btn-info"
+            onClick={this.handlePrevEvent}
+            disabled={this.state.page <= 1}
+          >
+            &#60; Previous
+          </button>
+          <p>{this.state.page}</p>
+          <button
+            type="button"
+            className="btn btn-info"
+            onClick={this.handleNextEvent}
+            disabled={this.state.page >= Math.ceil(this.state.totalArticles / 10)}
+          >
+            Next &#62;
+          </button>
+        </div>
+        <br />
+        <br />
       </div>
     );
   }
