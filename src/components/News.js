4332af14941efc527e72470097b4482338fdd169
Neeraj Keriya <neerajkeriya@gmail.com>
2022-05-13 12:36:59 +0530

    Added Infinity scroll feature

--- src/components/News.js
+++ src/components/News.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
-import NextPrevButton from "./NextPrevButton";
+// import NextPrevButton from "./NextPrevButton";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default class News extends Component {
   static defaultProps = {
@@ -19,7 +20,7 @@ export default class News extends Component {
     super(props);
     this.state = {
       articles: [],
-      loading: false,
+      loading: true,
       page: 1,
       totalResults: 0,
     };
@@ -48,46 +49,69 @@ export default class News extends Component {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
 
-  handlePrevEvent = () => {
-    let pagev = this.state.page - 1;
-    this.setState({
-      page: pagev,
-    });
-    this.updateNewsCard(pagev);
-  };
+  // handlePrevEvent = () => {
+  //   let pagev = this.state.page - 1;
+  //   this.setState({
+  //     page: pagev,
+  //   });
+  //   this.updateNewsCard(pagev);
+  // };
 
-  handleNextEvent = () => {
+  // handleNextEvent = () => {
+  //   let pagev = this.state.page + 1;
+  //   this.setState({
+  //     page: pagev,
+  //   });
+  //   this.updateNewsCard(pagev);
+  // };
+
+  fetchMoreData = async () => {
     let pagev = this.state.page + 1;
+    this.setState({ page: pagev });
+
+    const url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=72638e89521642eeae6d41e5f196d221&category=${this.props.category}&pageSize=${this.props.pageSize}&page=${pagev}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
     this.setState({
-      page: pagev,
+      articles: this.state.articles.concat(parsedData.articles),
+      totalResults: parsedData.totalResults,
     });
-    this.updateNewsCard(pagev);
   };
 
   render() {
     return (
-      <div className="container my-2">
+      <>
         <h1 className="text-center m-4">Top headlines...</h1>
         {this.state.loading && <Spinner />}
-        <div className="row">
-          {!this.state.loading &&
-            this.state.articles.map((element) => {
-              return (
-                <div key={element.url} className="col-md-4 my-2">
-                  <NewsItem
-                    title={element.title ? element.title : ""}
-                    description={element.description ? element.description : ""}
-                    imageUrl={element.urlToImage ? element.urlToImage : ""}
-                    newsUrl={element.url}
-                    author={element.author ? element.author : "unknown"}
-                    date={element.publishedAt}
-                    source={element.source.name}
-                  />
-                </div>
-              );
-            })}
-        </div>
-        <NextPrevButton
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner />}
+        >
+          <div className="container">
+            <div className="row">
+              {this.state.articles.map((element) => {
+                return (
+                  <div key={element.url} className="col-md-4 my-2">
+                    <NewsItem
+                      title={element.title ? element.title : ""}
+                      description={
+                        element.description ? element.description : ""
+                      }
+                      imageUrl={element.urlToImage ? element.urlToImage : ""}
+                      newsUrl={element.url}
+                      author={element.author ? element.author : "unknown"}
+                      date={element.publishedAt}
+                      source={element.source.name}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </InfiniteScroll>
+        {/* <NextPrevButton
           prevClickedEvent={this.handlePrevEvent}
           nextClickedEvent={this.handleNextEvent}
           disablePrev={this.state.page <= 1}
@@ -95,8 +119,8 @@ export default class News extends Component {
             this.state.page >= Math.ceil(this.state.totalResults / 10)
           }
           pageNumber={this.state.page}
-        />
-      </div>
+        /> */}
+      </>
     );
   }
 }
